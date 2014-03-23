512c776b5cc35113be1d78468447c6394d7db4fb
Austin Diviness <adiviness@mines.edu>
2014-03-22 18:57:21 -0600

    fixed one store test

--- src/unit10/pet2/test/controllers/store_controller_test.rb
+++ src/unit10/pet2/test/controllers/store_controller_test.rb
@@ -5,13 +5,13 @@ class StoreControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_select '#columns #side a', minimum: 3
-    assert_select '#main .entry', 3
+    assert_select '#main .entry', 1
     assert_select 'h3', 'Thumper'
   end
 
   test "markup needed for store.js.coffee is in place" do
     get :index
-    assert_select '.store .entry > img', 3
-    assert_select '.entry input[type=submit]', 3
+    assert_select '.store .entry > img', 1
+    assert_select '.entry input[type=submit]', 2
   end
 end
