9f143de770b5d744bf166f02ce1189d6e3dd1453
Austin Diviness <adiviness@mines.edu>
2014-03-22 18:33:12 -0600

    migrated animals to add gender

--- src/unit10/pet2/db/schema.rb
+++ src/unit10/pet2/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140320222248) do
+ActiveRecord::Schema.define(version: 20140323002554) do
 
   create_table "animals", force: true do |t|
     t.string   "name"
