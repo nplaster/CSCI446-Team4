9556b54eccb89944dd441b184eb26f3d85f15157
Naomi Plasterer <nplaster@mines.edu>
2014-04-02 17:21:43 -0600

    issues fixed for now

--- src/unit12/pet2/app/controllers/users_controller.rb
+++ src/unit12/pet2/app/controllers/users_controller.rb
@@ -1,14 +1,21 @@
 class UsersController < ApplicationController
+  
   def new
     @user = User.new
   end
   
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_parms)
     if @user.save
       redirect_to root_url, :notice => "Signed up!"
     else
       render :new
     end
+  end
+  
+  private
+  
+  def user_parms
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
