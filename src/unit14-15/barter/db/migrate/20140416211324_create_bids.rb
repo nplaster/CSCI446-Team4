c96a732a5c1f7f554dbea9534fe8ddc262f9c92b
Austin Diviness <austin.diviness@gmail.com>
2014-04-16 15:22:37 -0600

    changed bid table to use ids

--- src/unit14-15/barter/db/migrate/20140416211324_create_bids.rb
+++ src/unit14-15/barter/db/migrate/20140416211324_create_bids.rb
@@ -1,8 +1,8 @@
 class CreateBids < ActiveRecord::Migration
   def change
     create_table :bids do |t|
-      t.references :listing_item, index: true
-      t.references :bid_item, index: true
+      t.integer  :listing_item_id
+      t.integer  :bid_item_id
 
       t.timestamps
     end
