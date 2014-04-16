cc29c7fbc310f51fa276ebe8197d021333d340b1
Austin Diviness <austin.diviness@gmail.com>
2014-04-16 15:35:14 -0600

    initialized sorcery

--- src/unit14-15/barter/db/schema.rb
+++ src/unit14-15/barter/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140416211324) do
+ActiveRecord::Schema.define(version: 20140416213436) do
 
   create_table "bids", force: true do |t|
     t.integer  "listing_item_id"
@@ -29,5 +29,38 @@ ActiveRecord::Schema.define(version: 20140416211324) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "transactions", force: true do |t|
+    t.integer  "listing_item_id"
+    t.integer  "bid_item_id"
+    t.integer  "listing_user_id"
+    t.integer  "bid_user_id"
+    t.integer  "listing_verify"
+    t.string   "bnteger"
+    t.integer  "bid_verify"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "email",                           null: false
+    t.string   "crypted_password",                null: false
+    t.string   "salt",                            null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "remember_me_token"
+    t.datetime "remember_me_token_expires_at"
+    t.string   "reset_password_token"
+    t.datetime "reset_password_token_expires_at"
+    t.datetime "reset_password_email_sent_at"
+    t.string   "activation_state"
+    t.string   "activation_token"
+    t.datetime "activation_token_expires_at"
+  end
+
+  add_index "users", ["activation_token"], name: "index_users_on_activation_token"
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["remember_me_token"], name: "index_users_on_remember_me_token"
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token"
 
 end
