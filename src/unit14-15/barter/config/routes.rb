5e688ad210b8e8b7435332f4575884e9dccaa44f
Naomi Plasterer <nplaster@mines.edu>
2014-04-21 14:17:36 -0600

    created loggin

--- src/unit14-15/barter/config/routes.rb
+++ src/unit14-15/barter/config/routes.rb
@@ -6,6 +6,13 @@ Barter::Application.routes.draw do
   resources :bids
 
   resources :items
+  
+  get "logout" => "sessions#destroy", :as => "logout"
+  get "login" => "sessions#new", :as => "login"
+  get "signup" => "users#new", :as => "signup"
+  
+  resources :users
+  resources :sessions
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
