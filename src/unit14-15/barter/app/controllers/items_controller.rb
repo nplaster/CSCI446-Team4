eb1fc76eab4dba35f801fe3c578996333c068b1c
Jason Steinberg <jsteinbe@mines.edu>
2014-04-21 16:41:57 -0600

    Fix destroy redirect

--- src/unit14-15/barter/app/controllers/items_controller.rb
+++ src/unit14-15/barter/app/controllers/items_controller.rb
@@ -59,7 +59,7 @@ class ItemsController < ApplicationController
   def destroy
     @item.destroy
     respond_to do |format|
-      format.html { redirect_to items_url }
+      format.html { redirect_to my_items_url }
       format.json { head :no_content }
     end
   end
