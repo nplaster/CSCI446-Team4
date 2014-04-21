d3303ab65e463618788a8c90e4f34ba6c3f4e329
Jason Steinberg <jsteinbe@mines.edu>
2014-04-21 15:58:35 -0600

    item listing modified

--- src/unit14-15/barter/db/seeds.rb
+++ src/unit14-15/barter/db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Item.delete_all
+Item.create!(
+  name: 'Thumper',
+  description:
+    %{<p>
+        Thumper is a big fun loving rabbit. If you get to know him you'll never be alone.
+      </p>},
+  image_url:   'bunny.jpg',
+  status: 'Available',
+  condition: 'New')
+
+Item.create!(
+    name: 'Cat',
+    description:
+    %{<p>
+        Cat is missing a leg, will try to find but not so good chance, just sayin'.
+      </p>},
+    image_url:   'cat.jpg',
+    status: 'In Use',
+    condition: 'Poor')
+
+Item.create!(
+    name: 'Fridge Cat',
+    description:
+        %{<p>
+        Cat likes stuff.
+      </p>},
+    image_url:   'cat2.jpg',
+    status: 'Available',
+    condition: 'Good')
+Item.create!(
+    name: 'Door Stop',
+    description:
+        %{<p>
+        Will not get pushed over
+      </p>},
+    image_url:   'elephant.jpg',
+    status: 'Available',
+    condition: 'Excellent')
