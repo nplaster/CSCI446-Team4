f5d8e90eaec245bcb03a6c48880960ff0fc35a76
Austin Diviness <adiviness@mines.edu>
2014-04-25 16:56:36 -0600

    transactions work

--- src/unit14-15/barter/app/controllers/transactions_controller.rb
+++ src/unit14-15/barter/app/controllers/transactions_controller.rb
@@ -35,8 +35,27 @@ class TransactionsController < ApplicationController
     @transaction.bid_verify = params[:bid_verify]
     respond_to do |format|
       if @transaction.save
+        bids = Bid.where(:listing_item_id => params[:listing_item_id])
+        
+        bids.each do  |b| 
+            it = Item.where(:id => b.bid_item_id)
+            it[0].status = "Available" 
+            it[0].save
+        end
+        
+        it = Item.where(:id => params[:listing_item_id])
+        it[0].status = "Sold"
+        it[0].save
+
+        it = Item.where(:id => params[:bid_item_id])
+        it[0].status = "Sold"
+        it[0].save
+
+        bids.each { |b| b.destroy } 
+
         format.html { redirect_to @transaction, notice: 'Transaction was successfully created.' }
         format.json { render action: 'show', status: :created, location: @transaction }
+        
       else
         format.html { render action: 'new' }
         format.json { render json: @transaction.errors, status: :unprocessable_entity }
@@ -76,7 +95,7 @@ class TransactionsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def transaction_params
-      #params.require(:transaction).permit(:listing_item_id, :bid_item_id, :listing_user_id, :bid_user_id, :listing_verify, :bid_verify)
-      params
+      params.require(:transaction).permit(:listing_item_id, :bid_item_id, :listing_user_id, :bid_user_id, :listing_verify, :bid_verify)
+      #params
     end
 end
